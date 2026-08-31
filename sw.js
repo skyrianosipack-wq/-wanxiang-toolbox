14a21867af53e19bf1301f2b001503c4cdbc937e
skyrianosipack-wq <skyrianosipack@gmail.com>
2026-08-31 11:23:10 +0800

    V6：更新离线缓存版本

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wanxiang-v5';
+const CACHE='wanxiang-v6';
 const ASSETS=['./','./index.html','./style.css','./app.js','./addon.txt','./manifest.webmanifest','./icon.svg','./v2js1.txt','./v2js2.txt','./v2js3.txt','./v2js4.txt','./v2js5.txt'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
